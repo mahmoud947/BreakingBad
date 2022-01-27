bfb42e7ff22d078ba519f6f1975782261c507a90
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-01-27 09:49:06 +0200

    fix un save state issue with dagger hilt it's very good now

---
+++ app/src/main/java/com/example/breakingbad/ui/fragment/characters/navViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.breakingbad.ui.fragment.characters
+
+import android.os.Parcelable
+import androidx.lifecycle.ViewModel
+
+class navViewModel :ViewModel(){
+
+    var listState:Parcelable? = null
+}
